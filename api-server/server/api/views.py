f1ce5cc20f37feeaad32a2c5d783bc024a5bc719
Jongwoo Na <njw1204@naver.com>
2019-05-11 22:36:15 +0900

    Fix CORS policy error

--- api-server/server/api/views.py
+++ api-server/server/api/views.py
@@ -53,6 +53,8 @@ class Compile(View):
             # return response with json header
             response = HttpResponse(result, status=status_code)
             response["Content-Type"] = "application/json; charset=utf-8"
+            response["Access-Control-Allow-Origin"] = "*"
+            response["Access-Control-Allow-Methods"] = "*"
             return response
 
 
@@ -77,4 +79,6 @@ class CompileResult(View):
             # return response with json header
             response = HttpResponse(result, status=status_code)
             response["Content-Type"] = "application/json; charset=utf-8"
+            response["Access-Control-Allow-Origin"] = "*"
+            response["Access-Control-Allow-Methods"] = "*"
             return response
