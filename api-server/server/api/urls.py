ea67e0af64299988e1d85e7d33284f7f28f9c8cf
Jongwoo Na <njw1204@naver.com>
2019-05-11 11:49:44 +0900

    Add api-server main view

--- api-server/server/api/urls.py
+++ api-server/server/api/urls.py
@@ -1,4 +1,4 @@
-"""server URL Configuration
+"""api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path, reverse_lazy
-from django.views.generic.base import RedirectView
+from django.urls import path
+from . import views as api_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('', RedirectView.as_view(url=reverse_lazy("api-main")), name="index")
+    path("", api_views.Main.as_view(), name="api-main"),
 ]
--- api-server/server/api/views.py
+++ api-server/server/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.views.generic.base import View
 
-# Create your views here.
+
+class Main(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Welcome to API")
