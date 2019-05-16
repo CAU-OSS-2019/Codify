ca0eb366ea2803a1f67caa538921732d9f6dd690
Jongwoo Na <njw1204@naver.com>
2019-05-16 15:52:32 +0000

    Add stdin field to compile api

--- api-server/server/api/models.py
+++ api-server/server/api/models.py
@@ -13,6 +13,7 @@ class Source(models.Model):
 
     lang = models.CharField(max_length=10, choices=LANG_CHOICES)
     code = models.TextField(blank=True)
+    stdin = models.TextField(blank=True)
     output = models.TextField(blank=True)
     status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=3)
     created_date = models.DateTimeField(auto_now_add=True)
