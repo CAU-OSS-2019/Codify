5e14b6879183dcc7f4fef2f9e6448273bc08125e
Jongwoo Na <njw1204@naver.com>
2019-05-20 17:40:37 +0900

    Add source code download func

--- Codify-cleaned/js/codify/util.js
+++ Codify-cleaned/js/codify/util.js
@@ -48,4 +48,12 @@ function noti(msg, callback) {
 }
 
 
-export { replaceHTMLSpacing, changePopup, getAllChildTextNodes, createElementFromHTML, noti };
+function guid() {
+    function s4() {
+        return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+
+export { replaceHTMLSpacing, changePopup, getAllChildTextNodes, createElementFromHTML, noti, guid };
