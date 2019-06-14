7fa4787c7c9b459f44be3b65925f52efdf56b02f
ATEZ <shieldnet64@gmail.com>
2019-06-14 20:55:47 +0900

    MIDDLE WARE bracket missed. added.

--- api-server/server/server/settings.py
+++ api-server/server/server/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'api',
 ]
 
-MIDDLEWARE 
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
